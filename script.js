aea3c69c9247520bfd57af30f9b58033b6879d48
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-22 21:02:36 -0400

    built post api for email submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function retrieve(id) {
     const input = document.getElementById(id)
-    let email = input.value
-    console.log(email);
+    let usremail = input.value
+    const data = {email: usremail}
+
+    fetch(
+        'https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/items',
+        {method: 'POST',
+        header: {
+            'Content-Type': 'applications/json'
+        },
+        body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data );
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    })
 }
