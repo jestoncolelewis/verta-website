4fb92cd560f09d07305686fd6d31caaff3f1c4be
Jeston Cole Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2023-03-23 11:30:11 -0400

    created temp s3 function

--- IaC/functions.py
+++ IaC/functions.py
@@ -7,19 +7,29 @@ lamb = boto3.client('lambda')
 api = boto3.client('api')
 ses = boto3.client('sesv2')
 
-def build_s3():
+def build_s3(name, path, key):
+    try:
+        s3.create_bucket(
+            Bucket = name,
+            CreateBucketConfiguration = {
+                'LocationConstraint': 'us-west-2'
+            }
+        )
+        s3r.meta.client.upload_file(path, name, key)
+    except botocore.exceptions('ClientError') as err:
+        print('{}'.format(err.response['Error']['Message']))
+    response = s3.list_objects(Bucket = name)
+    objects = list(response.items())
+    file = objects[3][1][0].get('Key')
+    return file
+
+def build_lambda(name):
     try:
         return 'Success'
     except botocore.exceptions('ClientError') as err:
         return err
 
-def build_lambda():
-    try:
-        return 'Success'
-    except botocore.exceptions('ClientError') as err:
-        return err
-
-def build_api():
+def build_api(name):
     try:
         return 'Success'
     except botocore.exceptions('ClientError') as err:
